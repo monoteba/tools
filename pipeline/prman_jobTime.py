a09ada031598c12fe8426309f93f3d4dd2a3f86a
Morten Andersen <mortentoo@users.noreply.github.com>
2017-09-01 17:22:10 +0200

    Update prman_jobTime.py
    added progress info

--- pipeline/prman_jobTime.py
+++ pipeline/prman_jobTime.py
@@ -8,6 +8,7 @@ python prman_jobTime.py /path/to/jobFolder
 '''
 
 import os, sys
+from time import sleep
 import xml.etree.ElementTree as ET
 
 args = sys.argv[1:]
@@ -20,36 +21,53 @@ def readRenderTime(file):
 		return float(timer.text)
 
 
-if len(args) != 1:
-	print('You need to specify path as argument!')
-else:
-	rendertimes = []
-	for root, dirs, files in os.walk(args[0]):
-		for file in files:
-			if file.endswith(".xml"):
-				rendertimes.append(readRenderTime(os.path.join(root, file)))
+def updateProgress(count, total):
+	p = count / float(total) * 100
+	sys.stdout.write('\r')
+	sys.stdout.write("progress: %d%%" % (p))
+	sys.stdout.flush()
 
-	# header
-	print("\n%15s %10s %15s" % ("", "h:mm:ss", "seconds"))
 
-	# divider
-	print("------------------------------------------")
+def process():
+	if len(args) != 1:
+		print('You need to specify path as argument!')
+	else:
+		total = 0
+		xmlFiles = []
+		rendertimes = []
+		for root, dirs, files in os.walk(args[0]):
+			for file in files:
+				if file.endswith(".xml"):
+					xmlFiles.append(os.path.join(root, file))
+					total += 1
+			for i, path in enumerate(xmlFiles):
+				rendertimes.append(readRenderTime(path))
+				updateProgress(i + 1, total)
+					
 
-	# total in h:mm:ss format
-	secondsTotal = sum(rendertimes)
-	m, s = divmod(secondsTotal, 60)
-	h, m = divmod(m, 60)
-	print("%-15s|%4d:%02d:%02d|%15.3f" % ("job:", h, m, s, secondsTotal))
+		# header
+		print("\n%15s %10s %15s" % ("", "h.mm.ss", "seconds"))
 
-	# average in h:mm:ss format
-	secondsAvg = secondsTotal / len(rendertimes)
-	m, s = divmod(secondsAvg, 60)
-	h, m = divmod(m, 60)
-	print("%-15s|%4d:%02d:%02d|%15.3f" % ("average:", h, m, s, secondsAvg))
+		# divider
+		print("------------------------------------------")
 
-	# divider
-	print("------------------------------------------")
+		# total in h:mm:ss format
+		secondsTotal = sum(rendertimes)
+		m, s = divmod(secondsTotal, 60)
+		h, m = divmod(m, 60)
+		print("%-15s %4d.%02d.%02d %15.3f" % ("job:", h, m, s, secondsTotal))
 
-	# number of images
-	print("total images: %d\n" % (len(rendertimes)))
-	
+		# average in h:mm:ss format
+		secondsAvg = secondsTotal / len(rendertimes)
+		m, s = divmod(secondsAvg, 60)
+		h, m = divmod(m, 60)
+		print("%-15s %4d.%02d.%02d %15.3f" % ("average:", h, m, s, secondsAvg))
+
+		# divider
+		print("------------------------------------------")
+
+		# number of images
+		print("total images: %d\n" % (len(rendertimes)))
+
+
+process()
