c6e06ce5d28007d3d7ad2883eceb914566c4ebb5
Morten Andersen <mortentoo@users.noreply.github.com>
2017-09-04 08:18:57 +0200

    fixed wrong indentation, causing long run times

--- pipeline/prman_jobTime.py
+++ pipeline/prman_jobTime.py
@@ -13,60 +13,65 @@ import xml.etree.ElementTree as ET
 args = sys.argv[1:]
 
 def readRenderTime(file):
-    xml = ET.parse(file)
-    root = xml.getroot()
-    timers = root.findall(".//*[@name='totaltime']/elapsed")
-    for timer in timers:
-        return float(timer.text)
+	xml = ET.parse(file)
+	root = xml.getroot()
+	timers = root.findall(".//*[@name='totaltime']/elapsed")
+	for timer in timers:
+		return float(timer.text)
 
 
 def updateProgress(count, total):
-    p = count / float(total) * 100
-    sys.stdout.write('\r')
-    sys.stdout.write("processing files: %d%%" % (p))
-    sys.stdout.flush()
+	p = count / float(total) * 100
+	sys.stdout.write('\r')
+	sys.stdout.write("processing files: %d%%" % (p))
+	sys.stdout.flush()
 
 
 def process():
-    if len(args) != 1:
-        print('You need to specify path as argument!')
-    else:
-        total = 0
-        xmlFiles = []
-        rendertimes = []
-        for root, dirs, files in os.walk(args[0]):
-            for file in files:
-                if file.endswith(".xml"):
-                    xmlFiles.append(os.path.join(root, file))
-                    total += 1
-            for i, path in enumerate(xmlFiles):
-                rendertimes.append(readRenderTime(path))
-                updateProgress(i + 1, total)
-                    
+	if len(args) != 1:
+		print("You need to specify path as argument!")
+	else:
+		total = 0
+		xmlFiles = []
+		rendertimes = []
+		
+		for root, dirs, files in os.walk(args[0]):
+			for file in files:
+				if file.endswith(".xml"):
+					xmlFiles.append(os.path.join(root, file))
+					total += 1
+					
+		for i, path in enumerate(xmlFiles):
+			rendertimes.append(readRenderTime(path))
+			updateProgress(i + 1, total)
+		
+		if len(rendertimes) == 0:
+			print("No data found!")
+			return
 
-        # header
-        print("\n\n%15s %10s %15s" % ("", "h.mm.ss", "seconds"))
+		# header
+		print("\n\n%15s %10s %15s" % ("", "h.mm.ss", "seconds"))
 
-        # divider
-        print("------------------------------------------")
+		# divider
+		print("------------------------------------------")
 
-        # total in h:mm:ss format
-        secondsTotal = sum(rendertimes)
-        m, s = divmod(secondsTotal, 60)
-        h, m = divmod(m, 60)
-        print("%-15s %4d.%02d.%02d %15.3f" % ("job:", h, m, s, secondsTotal))
+		# total in h:mm:ss format
+		secondsTotal = sum(rendertimes)
+		m, s = divmod(secondsTotal, 60)
+		h, m = divmod(m, 60)
+		print("%-15s %4d:%02d:%02d %15.3f" % ("job:", h, m, s, secondsTotal))
 
-        # average in h:mm:ss format
-        secondsAvg = secondsTotal / len(rendertimes)
-        m, s = divmod(secondsAvg, 60)
-        h, m = divmod(m, 60)
-        print("%-15s %4d.%02d.%02d %15.3f" % ("average:", h, m, s, secondsAvg))
+		# average in h:mm:ss format
+		secondsAvg = secondsTotal / len(rendertimes)
+		m, s = divmod(secondsAvg, 60)
+		h, m = divmod(m, 60)
+		print("%-15s %4d:%02d:%02d %15.3f" % ("average:", h, m, s, secondsAvg))
 
-        # divider
-        print("------------------------------------------")
+		# divider
+		print("------------------------------------------")
 
-        # number of images
-        print("total images: %d\n" % (len(rendertimes)))
+		# number of images
+		print("total images: %d\n" % (len(rendertimes)))
 
 
 process()
